70f54d8b99eb0069d1de7d5975155c38ca95b7a9
Russell Ladd <russell.ladd@grl5.com>
2014-08-05 01:31:00 -0400

    Initial Commit

---
+++ Wolverine/Wolverine.h
@@ -0,0 +1,19 @@
+//
+//  Wolverine.h
+//  Wolverine
+//
+//  Created by Russell Ladd on 8/5/14.
+//  Copyright (c) 2014 GRL5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Wolverine.
+FOUNDATION_EXPORT double WolverineVersionNumber;
+
+//! Project version string for Wolverine.
+FOUNDATION_EXPORT const unsigned char WolverineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wolverine/PublicHeader.h>
+
+
