ec41b98b70e5f9088a5c4476528f8ebfa2a96b04
Russell Ladd <russell.ladd@grl5.com>
2014-08-14 22:54:54 -0400

    • Added fields to Person

--- Wolverine/People.swift
+++ Wolverine/People.swift
@@ -10,13 +10,21 @@ import Foundation
 
 public struct Person {
     
+    public let affiliation: [String]?
+    public let displayName: String?
+    public let email: String?
+    public let title: String?
+    public let uniqname: String?
+    public let workAddress: String? // convert " $ " to "\n"
+    public let workPhone: String? // convert "###/" to "(###) "
+    
     static func personWithJSONObject(jsonObject: AnyObject) -> PersonResult {
         
         if let dictionary = jsonObject as? NSDictionary {
             
             // Parse person
             
-            return .Success(Person())
+            return .Success(Person(affiliation: nil, displayName: nil, email: nil, title: nil, uniqname: nil, workAddress: nil, workPhone: nil))
         }
         
         return .Error(ErrorCode.InvalidJSON.error)
